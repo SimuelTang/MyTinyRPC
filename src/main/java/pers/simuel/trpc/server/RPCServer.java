308719a7b093310da5e4888bad018aa5aad16cb1
SimuelTang <xin1491101684@gmail.com>
2021-10-06 12:28:25 +0800

    add some configurations

--- src/main/java/pers/simuel/trpc/server/RPCServer.java
+++ src/main/java/pers/simuel/trpc/server/RPCServer.java
@@ -7,7 +7,6 @@ import pers.simuel.trpc.entity.RPCResponse;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
